56a753a4399d9d31d9513311979a61f3f1e5cb1f
reemhaitham12 <reemhaitham69@gmail.com>
2024-05-17 23:22:19 +0300

    add image of writer

--- js/main.js
+++ js/main.js
@@ -1,53 +1,80 @@
 const quotes = [
     {
         quote: `“Never trust anyone who has not brought a book with them.”`,
-        writer: `- Lemony Snicket`
+        writer: `- Lemony Snicket`,
+        photo:`./image/image14.jpg`
     },
     {
         quote: `“A room without books is like a body without a soul.”`,
-        writer: `- Marcus Tullius Cicero`
+        writer: `- Marcus Tullius Cicero`,
+        photo:`./image/image3.jpg`
     },
     {
         quote: `“It is better to be hated for what you are than to be loved for what you are not.”`,
-        writer: `- Andre Gide, Autumn Leaves`
+        writer: `- Andre Gide`,
+        photo:`./image/image13.jpg`
     },
     {
         quote: `“You only live once, but if you do it right, once is enough.”`,
-        writer: `- Mae West`
+        writer: `- Mae West`,
+        photo:`./image/image5.jpg`
     },
     {
         quote: `“Be the change that you wish to see in the world.”`,
-        writer: `- Mahatma Gandhi`
+        writer: `- Mahatma Gandhi`,
+        photo:`./image/image6.jpg`
     },
     {
         quote: `“If you tell the truth, you don't have to remember anything.”`,
-        writer: `- Mark Twain`
+        writer: `- Mark Twain`,
+        photo:`./image/image7.jpg`
     },
     {
         quote: `“A friend is someone who knows all about you and still loves you.”`,
-        writer: `- Elbert Hubbard`
+        writer: `- Elbert Hubbard`,
+        photo:`./image/image8.jpg`
     },
     {
         quote: `“Well done is better than well said.”`,
-        writer: `- Benjamin Franklin`
+        writer: `- Benjamin Franklin`,
+        photo:`./image/image9.jpg`
     },
     {
         quote: `“Be yourself; everyone else is already taken.”`,
-        writer: `- Oscar Wilde`
+        writer: `- Oscar Wilde`,
+        photo:`./image/image10.jpg`
     },
     {
         quote: `“It is during our darkest moments that we must focus to see the light.”`,
-        writer: `- Aristotle`
+        writer: `- Aristotle`,
+        photo:`./image/image11.jpg`
     },
     {
         quote: `“Do one thing every day that scares you.”`,
-        writer: `- Eleanor Roosevelt`
+        writer: `- Eleanor Roosevelt`,
+        photo:`./image/image12.jpg`
+    },
+    {
+        quote: `“So many books, so little time.”`,
+        writer: `- Frank Zappa`,
+        photo:`./image/image1.jpg`
+    },
+    {
+        quote: `“We accept the love we think we deserve.”`,
+        writer: `- Stephen Chbosky`,
+        photo:`./image/image15.jpg`
+    },
+    {
+        quote: `“Without music, life would be a mistake.”`,
+        writer: `- Friedrich Nietzsche`,
+        photo:`./image/image16.jpg`
     },
 ];
 
 let btn = document.getElementById("Qbtn");
 let Writer = document.getElementById("writer");
 let quote = document.getElementById("text");
+let imgWriter = document.getElementById("photo")
 let shuffledQuotes = quotes.slice(); 
 let currentIndex = shuffledQuotes.length;
 
@@ -66,4 +93,5 @@ btn.addEventListener("click", function () {
     currentIndex--;
     quote.innerHTML = shuffledQuotes[currentIndex].quote;
     Writer.innerHTML = shuffledQuotes[currentIndex].writer;
+    imgWriter.src=shuffledQuotes[currentIndex].photo;
 });
